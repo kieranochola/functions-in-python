a2d4905f9d80c092cd0e21c3c59dcb1837f62467
kieranochola <ocholadavidk@gmail.com>
2024-11-17 21:55:28 +0300

    functions

---
+++ main.py
@@ -0,0 +1,5 @@
+def printname(name,age):
+    print("hello i am",name,"and  i am ",age,"old")
+n=input("enter your name======= ")
+a=int(input("enter your age====="))
+printname(n,a)
---
+++ main2.py
@@ -0,0 +1,17 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+def div(x,y):
+    return x/y
+num1=int(input("enter number"))
+num2=int(input("enter number"))
+print("addition",add(num1,num2))
+
+print("sub",sub(num1,num2))
+
+print("mul",mul(num1,num2))
+
+print("div",div(num1,num2))
